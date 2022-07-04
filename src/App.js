fbb99d3d2762221708ea6608fa0595cd27c316cf
MD. Mehrab Hossain <38918084+jansir17@users.noreply.github.com>
2022-07-04 10:37:44 +0600

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import UseStateHook from "./Hooks/useStateHook";
 import MyUseEffectHook from "./Hooks/useEffectHook";
+import MyUseMemo from "./Hooks/useMemoHook";
 
 function App() {
   return (
     <>
     <UseStateHook startingValue={40} initialTheme='red'/>
     <MyUseEffectHook/>
+    <MyUseMemo/>
     </>
     );
 
